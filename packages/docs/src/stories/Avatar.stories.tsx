ae528c1f49a83894e90ceb329cfe9fe723f77476
felipetacara <felipetacara@gmail.com>
2022-10-14 16:01:39 -0300

    Update components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/FelipeTacara.png',
     alt: 'Felipe Tacara',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
